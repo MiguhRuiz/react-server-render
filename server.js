290597b49cf18b9408f21c8c267325a10845cc7e
Miguh Ruiz <miguhruiz@gmail.com>
2016-08-17 19:24:45 +0200

    [Add] Server component rendering on Node and static files with Express

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
 
 const app = express()
-
 const port = process.env.PORT || 5000
 
+app.use('/public', express.static('public'))
+
+import Component from './src/server/app.jsx'
+const App = renderToString(<Component />)
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send(App)
 })
 
 app.listen(port, () => {
--- src/server/app.jsx
+++ src/server/app.jsx
@@ -7,12 +7,12 @@ class MyAwesomeApp extends React.Component {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <link rel="stylesheet" href="https://miguhruiz.xyz/public/css/index.css" />
         <title>My awesome app rendered at the server</title>
       </head>
       <body>
-        <div class="react-app"></div>
+        <div className="react-app"></div>
         <script src="./public/js/app.js"></script>
       </body>
       </html>
